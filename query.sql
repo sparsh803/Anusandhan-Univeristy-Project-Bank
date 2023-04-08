1f318345c72ce29063aa18c645b9ad3a1c1e7be4
Sparsh Gupta <65289620+sparsh803@users.noreply.github.com>
2023-04-08 18:09:06 +0530

    added tempRecord & permRecord creation queries.
    d

--- query.sql
+++ query.sql
@@ -13,3 +13,29 @@ email varchar(255) unique not null,
 password varchar(255) not null,
 contact_number char(10) not null
 );
+
+
+create table tempRecords(
+ tokenID int primary key auto_increment,
+ studentID int not null,
+ projectTitle varchar(255) not null, 
+ status varchar(15) not null, 
+ projectAuthors varchar(255) not null,
+ projectKeywords varchar(255) not null,
+ projectDescription varchar(255) not null, 
+ filePath varchar(500) not null, 
+ foreign key (studentID) references users(id)
+);
+
+create table permRecords(
+ projectID int primary key auto_increment,
+ studentID int not null,
+ projectTitle varchar(255) not null,
+ projectAuthors varchar(255) not null,
+ projectKeywords varchar(255) not null, 
+ projectDescription varchar(255) not null, 
+ filePath varchar(500) not null,
+ foreign key (studentID) references users(id)
+);
+
+
