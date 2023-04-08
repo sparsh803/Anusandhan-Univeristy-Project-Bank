15164f1b0cea2ad2774752c9e7777cfa88158cab
AadiYNWA <97405355+AadiYNWA@users.noreply.github.com>
2023-04-08 17:53:50 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,15 @@
+create table users(
+id int primary key auto_increment,
+name varchar(255) not null,
+email varchar(255) unique not null,
+password varchar(255) not null,
+contact_number char(10) not null
+);
 
+create table admins(
+id int primary key auto_increment,
+name varchar(255) not null,
+email varchar(255) unique not null,
+password varchar(255) not null,
+contact_number char(10) not null
+);
